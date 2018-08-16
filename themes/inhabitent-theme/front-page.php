80e181ff90bf2620418d3e7358b81bbefe5badaf
Julia Browne <julia.browne@live.ca>
2018-08-16 16:06:11 -0700

    edit front-page.php and header.php to include new classes and images

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -2,64 +2,40 @@
 /**
  * The main template file.
  *
- * @package inhabitent_Theme
+ * @package Inhabitent_Theme
  * Template Name: Front Page
  */
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-  <main id="main" class="site-main" role="main">
+<div class="home-banner"> </div>
 
-    <?php if ( have_posts() ) : ?>
-
-    <?php if ( is_home() && ! is_front_page() ) : ?>
-    <header>
-      <h1 class="page-title screen-reader-text">
-        <?php single_post_title(); ?>
-      </h1>
-    </header>
-    <?php endif; ?>
-
-    <?php /* Start the Loop */ ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-
-    <?php get_template_part( 'template-parts/content' ); ?>
-
-    <?php endwhile; ?>
-
-    <?php the_posts_navigation(); ?>
-
-    <?php else : ?>
-
-    <?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-    <?php endif; ?>
-
-
-
-
+<h2>Shop Stuff</h2>
 
     <?php $terms = get_terms([
     'taxonomy' => 'product_type',
     'hide_empty' => false,
-		]); ?>
+    ]); ?>
 
     <div class="shop-stuff-container">
       <?php foreach ( $terms as $term ) : ?>
       <div class="shop-stuff">
-        <h2>
-          <?php echo $term->name; ?>
-        </h2>
-        <div class="shop-stuff-icon"><img src="<?php echo get_template_directory_uri()?>/images/product-type-icons/<?php echo $term->slug ?>.svg"></div>
+          <img class="shop-stuff-icon" src="<?php echo get_template_directory_uri()?>/images/product-type-icons/<?php echo $term->slug ?>.svg">
         <p>
           <?php echo $term->description; ?>
         </p>
+        <p>
+          <a class="shop-stuff-button" href="<?php echo get_term_link( $term ); ?>"> <?php echo $term->name; ?> Stuff </a>
+        </p>
       </div>
       <?php endforeach; ?>
+
     </div>
 
+    </section>
 
+
+<!-- Journal Posts --> 
     <?php
    $args = array( 'post_type' => 'post', 'order' => 'DESC' );
 	 $blog_posts = get_posts( $args ); // returns an array of posts ?>
