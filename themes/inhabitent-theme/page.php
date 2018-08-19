cec53278e57d2cfdfd98c3c99ada678970549bf3
Julia Browne <julia.browne@live.ca>
2018-08-19 15:34:22 -0700

    add sidebar code to page.templates, link published posts to journal page on website, edit single post page

--- themes/inhabitent-theme/page.php
+++ themes/inhabitent-theme/page.php
@@ -7,17 +7,42 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+ <?php
+   $args = array( 'post_type' => 'post', 'order' => 'DESC');
+	 $blog_posts = get_posts( $args ); // returns an array of posts ?>
+    
+    <div class="featured-post-container">
+    
+    <?php foreach ( $blog_posts as $post ) : setup_postdata( $post ); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+      <div class="featured-post">
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+      <div class="featured-post-thumbnail">
+      <?php the_post_thumbnail(); ?>
+      </div>  
 
-			<?php endwhile; // End of the loop. ?>
+      <section class="featured-post-info">
+        <div class="date-and-comment">
+        <?php inhabitent_posted_on(); ?> / 
+        <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>
+        </div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        <a href='<?php the_permalink(); ?>'>
+        <h2 class="featured-post-title">
+						<?php the_title(); ?>
+				</h2>
+				</a>	
+				<?php the_content(); ?>
+        <a class="read-entry-button" href='<?php the_permalink(); ?>'>Read Entry</a>
+        </a>
+    </section>  
+      </div>
+
+      <?php endforeach; wp_reset_postdata(); ?>
+    </div>
+
+
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
